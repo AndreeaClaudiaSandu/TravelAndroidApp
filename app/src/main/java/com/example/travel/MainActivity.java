244c485a58eb4435d0b06b35cff1659485e3c8e2
AndreeaClaudiaSandu <asandu766@gmail.com>
2022-05-09 01:36:29 +0300

    Images updates

--- app/src/main/java/com/example/travel/MainActivity.java
+++ app/src/main/java/com/example/travel/MainActivity.java
@@ -45,6 +45,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         toolbar.setTitle("Pick a city");
         replaceFragment(new FragmentPickCity());
+
     }
 
 
